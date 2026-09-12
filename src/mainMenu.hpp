2fd4c33c17b2090296a89f169ab933c1f13ed888
John McRae <jfmcrae@proton.me>
2026-09-12 15:12:47 -0400

    Added options to main menu

--- src/mainMenu.hpp
+++ src/mainMenu.hpp
@@ -3,4 +3,13 @@ using namespace std;
 
 void mainMenu() {
     cout << "-- Main Menu --\n";
+    cout << "1. Check for MIDI device\n";
+    cout << "2. Play single note loop\n";
+    cout << "3. Play sequence loop\n";
+    cout << "4. Show MIDI input\n";
+    cout << endl;
+    cout << "[x] exit\n";
+    auto userInput;
+    cin >> userInput;
+    cout << "Selected: " << userInput << endl;
 }
